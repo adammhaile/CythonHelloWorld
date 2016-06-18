2b38db415366e93e577ee043b4e7279eb61ac036
adammhaile <adammhaile@gmail.com>
2016-06-18 19:20:15 -0400

    adding C++11

--- Test.h
+++ Test.h
@@ -1,7 +1,33 @@
+#include <iostream>
+#include <string>
 class Test {
 public:
     int _x;
     Test();
     ~Test();
+    std::string str = R"(A\nB\nC\n)";
     int calc(int x);
 };
+
+Test::Test() {}
+
+Test::~Test() { }
+
+int Test::calc(int x) {
+    auto q = nullptr;
+    std::string test;
+    std::cout << str << std::endl;
+    int ar[5] = {1, 2, 3, 4, 5};
+    for(auto &i: ar){
+        x += i;
+    }
+
+    if (x>1 and x>2 or x>3)
+    {
+        std::cout << "testing";
+    }
+
+    std::string hel = "hello";
+    decltype(hel) wor = "world";
+    return x;
+}
--- setup.py
+++ setup.py
@@ -1,8 +1,58 @@
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
+import setuptools.extension, Cython.Build
+from setuptools.command.build_ext import build_ext as _build_ext
 
-setup(ext_modules = cythonize(
-           "main.pyx",                 # our Cython source
-           sources=["Test.cpp"],  # additional source file(s)
-           language="c++",             # generate C++ code
-      ))
+import Cython.Compiler.Options
+
+COMPILE_ARGS = [
+    '-DNDEBUG',
+    '-DWINDOWS',
+    '/Dand=&&',
+    '/Dnot=!',
+    '/Dor=||',
+    '/Duint=size_t',
+    ]
+
+class build_ext(_build_ext):
+    # See https://groups.google.com/forum/#!topic/cython-users/IZMENRz6__s
+    def finalize_options(self):
+        extension = setuptools.extension.Extension(
+            name='main',
+            sources=['main.pyx'],
+            #libraries=LIBRARIES,
+            #include_dirs=['src'],
+            extra_compile_args=COMPILE_ARGS,
+            language='c++',
+            )
+
+        module = Cython.Build.cythonize(
+            [extension],
+            language='c++',
+            language_level=3,
+
+            compiler_directives=dict(
+                c_string_encoding='ascii',
+                # c_string_type='unicode', # Why doesn't this work?
+                )
+            )
+        self.distribution.ext_modules = module
+        super(build_ext, self).finalize_options()
+
+# setup(ext_modules = cythonize(
+#            "main.pyx",                 # our Cython source
+#            sources=["Test.h"],  # additional source file(s)
+#            language="c++",             # generate C++ code
+#            extra_compile_args=COMPILE_ARGS
+#            cmdclass={
+#                'build_ext': build_ext,
+#                'clean': Clean,
+#                'generate': Generate,
+#                'local': Local,
+#                },
+#       ))
+
+setuptools.setup(
+    #name='test',
+    cmdclass={
+        'build_ext': build_ext,
+        },
+    )
